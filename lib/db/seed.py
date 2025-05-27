b665520584aedb9e48205a93241182cc7ed58692
Luke Kiprop <luke.rono@student.moringaschool.com>
2025-05-27 10:55:55 +0300

    Updated seed.py

--- lib/db/seed.py
+++ lib/db/seed.py
@@ -1,3 +1,8 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from lib.db.models import Base, Author, Book
+
+# Create a database engine
+engine = create_engine('sqlite:///lib/db/library.db')
+Session = sessionmaker(bind=engine)
+session = Session()
