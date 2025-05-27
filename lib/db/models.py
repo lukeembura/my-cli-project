f056affc509ea89e5ac9143ef8536bd0b22e8c4b
Luke Kiprop <luke.rono@student.moringaschool.com>
2025-05-27 09:42:44 +0300

    Updated model.py

--- lib/db/models.py
+++ lib/db/models.py
@@ -3,27 +3,7 @@
     
     
     
-    @validates('title')
-    def validate_title(self, key, value):
-        if not value or len(value.strip()) == 0:
-            raise ValueError("Book title cannot be empty.")
-        return value
-
-    @classmethod
-    def create(cls, session, title, author):
-        book = cls(title=title, author=author)
-        session.add(book)
-        session.commit()
-        return book
-
-    @classmethod
-    def get_all(cls, session):
-        return session.query(cls).all()
-
-    @classmethod
-    def find_by_id(cls, session, id):
-        return session.query(cls).get(id)
-
+    
     def delete(self, session):
         session.delete(self)
         session.commit()
